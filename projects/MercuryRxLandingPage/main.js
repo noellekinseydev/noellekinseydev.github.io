020993d1a5744f2059bf37cc95dfc331d316290e
Noelle Horton <49262786+noellekinseydev@users.noreply.github.com>
2020-02-16 09:01:14 -0800

    Update

--- projects/MercuryRxLandingPage/main.js
+++ projects/MercuryRxLandingPage/main.js
@@ -17,4 +17,36 @@ document.addEventListener("DOMContentLoaded", function() {
     $(".minutes").text(duration.minutes());
     $(".seconds").text(duration.seconds());
   }, interval);
+
+  // Transition effect for navbar
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 50) {
+      $(".navbar").addClass("bg-gradient");
+    } else {
+      $(".navbar").removeClass("bg-gradient");
+    }
+  });
+
+  // handle links with @href started with '#' only
+  $(document).on("click", 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr("href");
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+      return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+    console.log(id);
+
+    // animated top scrolling
+    $("body, html").animate({ scrollTop: pos });
+  });
 });
